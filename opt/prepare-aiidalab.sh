f0b6033da9139af78b54d5917a2105145b8a5ee1
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2020-01-31 08:16:31 +0100

    Base aiidalab on aiida-core docker container. (#30)
    Docker file is now significantly shortened, as it is now based on
    `aiidateam/aiida-core` container where most aiida-related things are
    handled. Most of the startup scripts are removed since, again, they
    are already present in the aiida-core image. Python2 support is
    completely removed. Added two environment variables: `AIIDALAB_HOME`
    and `AIIDALAB_APPS` used by the aiidalab home app. The `fix-permissions`
    script was removed since all the user setup stuff happens inside
    startup scripts that are run by a non-root user.  The script to launch
    Jupyter notebook server was kept in services since it must be run
    after all scripts in `/etc/my_init.d` folder (also after `finalize_init.sh`),
    and, quite importantly, Jupyter should always be on. The `.travis` file
    was slightly simplified, `sleep 15` call was replaced by `wait-for-services`.

---
+++ opt/prepare-aiidalab.sh
@@ -0,0 +1,88 @@
+#!/bin/bash -e
+
+# Debugging.
+set -x
+
+# Environment.
+export SHELL=/bin/bash
+
+
+# Setup CP2K code.
+computer_name=localhost
+code_name=cp2k
+verdi code show ${code_name}@${computer_name} || verdi code setup \
+    --non-interactive                                             \
+    --label ${code_name}                                          \
+    --description "cp2k on this computer"                         \
+    --input-plugin cp2k                                           \
+    --computer ${computer_name}                                   \
+    --remote-abs-path `which cp2k.popt`
+
+# Setup Quantum ESPRESSO pw.x code.
+code_name=pw
+verdi code show ${code_name}@${computer_name} || verdi code setup \
+    --non-interactive                                             \
+    --label ${code_name}                                          \
+    --description "pw.x on this computer"                         \
+    --input-plugin quantumespresso.pw                             \
+    --computer ${computer_name}                                   \
+    --remote-abs-path `which pw.x`
+
+# Setup pseudopotentials.
+if [ ! -e /home/$SYSTEM_USER/SKIP_IMPORT_PSEUDOS ]; then
+      cd /opt/pseudos
+      verdi data upf listfamilies | grep 'SSSP_efficiency_v1.0'|| verdi data upf uploadfamily SSSP_efficiency_pseudos 'SSSP_efficiency_v1.0' 'SSSP pseudopotential library'
+      verdi data upf listfamilies | grep 'SSSP_precision_v1.0' || verdi data upf uploadfamily SSSP_precision_pseudos 'SSSP_precision_v1.0' 'SSSP pseudopotential library'
+fi
+
+# Setup AiiDA jupyter extension.
+# Don't forget to copy this file to .ipython/profile_default/startup/
+# aiida/tools/ipython/aiida_magic_register.py
+if [ ! -e /home/$SYSTEM_USER/.ipython/profile_default/startup/aiida_magic_register.py ]; then
+   mkdir -p /home/$SYSTEM_USER/.ipython/profile_default/startup/
+   cat << EOF > /home/$SYSTEM_USER/.ipython/profile_default/startup/aiida_magic_register.py
+if __name__ == "__main__":
+
+    try:
+        import aiida
+        del aiida
+    except ImportError:
+        pass
+    else:
+        import IPython
+        # pylint: disable=ungrouped-imports
+        from aiida.tools.ipython.ipython_magics import load_ipython_extension
+
+        # Get the current Ipython session
+        IPYSESSION = IPython.get_ipython()
+
+        # Register the line magic
+        load_ipython_extension(IPYSESSION)
+EOF
+fi
+
+# Install/upgrade apps.
+if [ ! -e /home/$SYSTEM_USER/apps ]; then
+   mkdir /home/$SYSTEM_USER/apps
+   touch /home/$SYSTEM_USER/apps/__init__.py
+   git clone https://github.com/aiidalab/aiidalab-home /home/$SYSTEM_USER/apps/home
+   cd /home/$SYSTEM_USER/apps/home
+   git checkout aiida_v1.0
+   cd -
+   echo '{
+  "hidden": [],
+  "order": [
+    "aiida-tutorials",
+    "cscs",
+    "calcexamples"
+  ]
+}' > /home/$SYSTEM_USER/apps/home/.launcher.json
+   git clone https://github.com/aiidalab/aiidalab-widgets-base /home/$SYSTEM_USER/apps/aiidalab-widgets-base
+   cd /home/$SYSTEM_USER/apps/aiidalab-widgets-base
+   git checkout aiida-1.0
+   cd -
+   git clone https://github.com/aiidalab/aiidalab-calculation-examples.git /home/$SYSTEM_USER/apps/calcexamples
+   cd /home/$SYSTEM_USER/apps/calcexamples
+   git checkout aiida-1.0
+   cd -
+fi
