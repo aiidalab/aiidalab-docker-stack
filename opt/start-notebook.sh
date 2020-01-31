8897598e406ed432288cbc764c3de683aee04a34
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2020-01-31 08:16:31 +0100

    Base aiidalab on aiida-core docker container. (#30)
    Docker file is now significantly shortened, as it is now based on
    `aiidateam/aiida-core` container where most aiida-related things are
    handled. Most of the startup scripts are removed since, again, they
    are already present in the aiida-core image. Python2 support is
    completely removed. Added two environment variables: `AIIDALAB_HOME`
    and `AIIDALAB_APPS` used by the aiidalab home app. The `fix-permissions`
    script was removed since all the user setup stuff happens inside
    startup scripts that are run by a non-root user.  The script to launch
    Jupyter notebook server was kept in services since it must be run
    after all scripts in `/etc/my_init.d` folder (also after `finalize_init.sh`),
    and, quite importantly, Jupyter should always be on. The `.travis` file
    was slightly simplified, `sleep 15` call was replaced by `wait-for-services`.

--- opt/start-notebook.sh
+++ opt/start-notebook.sh
@@ -1,15 +1,17 @@
 #!/bin/bash -e
 
+# Debugging.
 set -x
 
-# environment
-export PYTHONPATH=/project
+# Environment.
 export SHELL=/bin/bash
 
-cd /project
-/opt/aiidalab-jupyterhub-singleuser                                \
+
+# Enter home folder and start jupyterhub-singleuser.
+cd /home/${SYSTEM_USER}
+/opt/aiidalab-singleuser                                           \
     --ip=0.0.0.0                                                   \
     --port=8888                                                    \
-    --notebook-dir="/project"                                      \
+    --notebook-dir="/home/${SYSTEM_USER}"                          \
     --NotebookApp.iopub_data_rate_limit=1000000000                 \
     --NotebookApp.default_url="/apps/apps/home/start.ipynb"
