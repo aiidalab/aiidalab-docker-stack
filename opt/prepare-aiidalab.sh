dac49eb17c025d662bfa5698140f003e811c3336
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2020-02-13 17:08:42 +0100

    Work on the packages installed by default: (#41)
    * Add DEFAULT_APP_GIT_BRANCH environment variable which specifies
    the default git branch used when installing a new AiiDA lab app.
    
    * Modify the list of packages installed by default. Now the list is: home,
    aiidalab-widgets-base, quantum-espresso

--- opt/prepare-aiidalab.sh
+++ opt/prepare-aiidalab.sh
@@ -41,7 +41,7 @@ verdi code show ${code_name}@${computer_name} || verdi code setup \
     --remote-abs-path `which pw.x`
 
 # Setup pseudopotentials.
-if [ ! -e /home/$SYSTEM_USER/SKIP_IMPORT_PSEUDOS ]; then
+if [ ! -e /home/${SYSTEM_USER}/SKIP_IMPORT_PSEUDOS ]; then
    verdi data upf listfamilies | grep 'SSSP_efficiency_v1.0'|| verdi import -n /opt/pseudos/SSSP_efficiency_pseudos.aiida
    verdi data upf listfamilies | grep 'SSSP_precision_v1.0' || verdi import -n /opt/pseudos/SSSP_precision_pseudos.aiida
 fi
@@ -49,9 +49,9 @@ fi
 # Setup AiiDA jupyter extension.
 # Don't forget to copy this file to .ipython/profile_default/startup/
 # aiida/tools/ipython/aiida_magic_register.py
-if [ ! -e /home/$SYSTEM_USER/.ipython/profile_default/startup/aiida_magic_register.py ]; then
-   mkdir -p /home/$SYSTEM_USER/.ipython/profile_default/startup/
-   cat << EOF > /home/$SYSTEM_USER/.ipython/profile_default/startup/aiida_magic_register.py
+if [ ! -e /home/${SYSTEM_USER}/.ipython/profile_default/startup/aiida_magic_register.py ]; then
+   mkdir -p /home/${SYSTEM_USER}/.ipython/profile_default/startup/
+   cat << EOF > /home/${SYSTEM_USER}/.ipython/profile_default/startup/aiida_magic_register.py
 if __name__ == "__main__":
 
     try:
@@ -73,27 +73,34 @@ EOF
 fi
 
 # Install/upgrade apps.
-if [ ! -e /home/$SYSTEM_USER/apps ]; then
-   mkdir /home/$SYSTEM_USER/apps
-   touch /home/$SYSTEM_USER/apps/__init__.py
-   git clone https://github.com/aiidalab/aiidalab-home /home/$SYSTEM_USER/apps/home
-   cd /home/$SYSTEM_USER/apps/home
-   git checkout develop
-   cd -
-   echo '{
-  "hidden": [],
-  "order": [
-    "aiida-tutorials",
-    "cscs",
-    "calcexamples"
-  ]
-}' > /home/$SYSTEM_USER/apps/home/.launcher.json
-   git clone https://github.com/aiidalab/aiidalab-widgets-base /home/$SYSTEM_USER/apps/aiidalab-widgets-base
-   cd /home/$SYSTEM_USER/apps/aiidalab-widgets-base
-   git checkout develop
-   cd -
-   git clone https://github.com/aiidalab/aiidalab-calculation-examples.git /home/$SYSTEM_USER/apps/calcexamples
-   cd /home/$SYSTEM_USER/apps/calcexamples
-   git checkout develop
-   cd -
+if [ ! -e /home/${SYSTEM_USER}/apps ]; then
+
+  # Create apps folder and make it importable from python.
+  mkdir /home/${SYSTEM_USER}/apps
+  touch /home/${SYSTEM_USER}/apps/__init__.py
+
+  # First install the home app.
+  git clone https://github.com/aiidalab/aiidalab-home /home/${SYSTEM_USER}/apps/home
+  cd /home/${SYSTEM_USER}/apps/home
+  git checkout ${DEFAULT_APP_GIT_BRANCH}
+  cd -
+
+  # Define the order how the apps should appear.
+  echo '{
+    "hidden": [],
+    "order": [
+      "aiidalab-widgets-base",
+      "quantum-espresso"
+    ]
+  }' > /home/${SYSTEM_USER}/apps/home/.launcher.json
+
+  git clone https://github.com/aiidalab/aiidalab-widgets-base /home/${SYSTEM_USER}/apps/aiidalab-widgets-base
+  cd /home/${SYSTEM_USER}/apps/aiidalab-widgets-base
+  git checkout ${DEFAULT_APP_GIT_BRANCH}
+  cd -
+
+  git clone https://github.com/aiidalab/aiidalab-qe.git /home/${SYSTEM_USER}/apps/quantum-espresso
+  cd /home/${SYSTEM_USER}/apps/quantum-espresso
+  git checkout ${DEFAULT_APP_GIT_BRANCH}
+ cd -
 fi
