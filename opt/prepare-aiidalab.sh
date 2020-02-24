bcc06a575fb30e9958929b2a3237b77a36b09a6f
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2020-02-24 10:03:09 +0100

    Remove conda activattion from opt/prepare-aiidalab.sh script. (#46)

--- opt/prepare-aiidalab.sh
+++ opt/prepare-aiidalab.sh
@@ -6,19 +6,6 @@ set -x
 # Environment.
 export SHELL=/bin/bash
 
-# Activating conda
-__conda_setup="$('/opt/conda/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
-if [ $? -eq 0 ]; then
-    eval "$__conda_setup"
-else
-    if [ -f "/opt/conda/etc/profile.d/conda.sh" ]; then
-        . "/opt/conda/etc/profile.d/conda.sh"
-    else
-        export PATH="/opt/conda/bin:$PATH"
-    fi
-fi
-unset __conda_setup
-
 # Setup CP2K code.
 computer_name=localhost
 code_name=cp2k
