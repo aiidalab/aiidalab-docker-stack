69c7cff049f1cfa539b0b1728e5e08cbf35e398d
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2020-04-27 00:17:48 +0200

    Fix run_aiidalab.sh for Ubuntu (#65)

--- run_aiidalab.sh
+++ run_aiidalab.sh
@@ -13,7 +13,7 @@ fi
 PORT=${1}
 FOLDER=${2}
 TOKEN=`openssl rand -hex 32`
-IMAGE='aiidalab/aiidalab-docker-stack:latest'
+IMAGE='aiidalab/aiidalab-docker-stack'
 
 echo "Pulling the image from the Docker Hub..."
 docker pull ${IMAGE}
