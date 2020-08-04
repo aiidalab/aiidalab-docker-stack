dd83b13309f17186dfdaec02369dcb52bb3ebf5e
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2020-08-04 17:07:04 +0200

    Remove pw and cp2k code setup. (#93)
    It is hard to generalize code setup in the docker image, therefore,
    this task is transferred to the apps to perform the setup on per
    request basis.

--- opt/prepare-aiidalab.sh
+++ opt/prepare-aiidalab.sh
@@ -6,27 +6,6 @@ set -x
 # Environment.
 export SHELL=/bin/bash
 
-# Setup CP2K code.
-computer_name=localhost
-code_name=cp2k
-verdi code show ${code_name}@${computer_name} || verdi code setup \
-    --non-interactive                                             \
-    --label ${code_name}                                          \
-    --description "cp2k v5.1 in AiiDA lab container."             \
-    --input-plugin cp2k                                           \
-    --computer ${computer_name}                                   \
-    --remote-abs-path `which cp2k.popt`
-
-# Setup Quantum ESPRESSO pw.x code.
-code_name=pw
-verdi code show ${code_name}@${computer_name} || verdi code setup \
-    --non-interactive                                             \
-    --label ${code_name}                                          \
-    --description "pw.x v.6.0 in AiiDA lab container."            \
-    --input-plugin quantumespresso.pw                             \
-    --computer ${computer_name}                                   \
-    --remote-abs-path `which pw.x`
-
 # Setup pseudopotentials.
 if [ ! -e /home/${SYSTEM_USER}/SKIP_IMPORT_PSEUDOS ]; then
    verdi data upf listfamilies | grep 'SSSP_1.1_efficiency'|| verdi import -n /opt/pseudos/SSSP_efficiency_pseudos.aiida
