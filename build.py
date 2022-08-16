bf52480f4634c33d3e6f27a5d5126f9ab5c43f5a
Simon Adorf <simon.adorf@epfl.ch>
2022-08-16 17:24:18 +0200

    Revise versioning and tagging.

---
+++ build.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+from pathlib import Path
+
+import click
+from yaml import SafeLoader, load
+
+BUILD_CONFIG = load(Path("build.yml").read_text(), Loader=SafeLoader)
+
+
+def get_organization():
+    return BUILD_CONFIG["organization"]
+
+
+def get_version():
+    return BUILD_CONFIG["version"]
+
+
+def get_tags():
+    yield BUILD_CONFIG["version"]  # The version of the stack.
+    # The versions of dependencies:
+    for name, version in BUILD_CONFIG["versions"].items():
+        yield f"{name}-{version}"
+
+
+def get_docker_build_args():
+    yield f"VERSION={BUILD_CONFIG['version']}"
+    for name, version in BUILD_CONFIG["versions"].items():
+        yield f"{name.upper()}_VERSION={version}"
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.option(
+    "--github-actions",
+    is_flag=True,
+    help="Output tags in a format convenient for GitHub actions.",
+)
+def tags(github_actions):
+    if github_actions:
+        click.echo(r"%0A".join(f"type=raw,event=tag,value={tag}" for tag in get_tags()))
+    else:
+        click.echo("\n".join(get_tags()))
+
+
+@cli.command()
+@click.option(
+    "--github-actions",
+    is_flag=True,
+    help="Output tags in a format convenient for GitHub actions.",
+)
+def docker_build_args(github_actions):
+    if github_actions:
+        click.echo(r"%0A".join(get_docker_build_args()))
+    else:
+        click.echo(" ".join(f"--build-arg {arg}" for arg in get_docker_build_args()))
+
+
+if __name__ == "__main__":
+    cli()
--- dodo.py
+++ dodo.py
@@ -1,7 +1,6 @@
 from pathlib import Path
 
-ORG = "aiidalab"
-VER = "latest"
+from build import get_docker_build_args, get_organization, get_tags, get_version
 
 DOIT_CONFIG = {"default_tasks": ["build"]}
 
@@ -9,16 +8,26 @@ DOIT_CONFIG = {"default_tasks": ["build"]}
 def task_build():
     """Build all docker images."""
 
-    contexts = [p.parent for p in Path("stack").glob("*/Dockerfile")]
+    contexts = [p.parent for p in sorted(Path("stack").glob("*/Dockerfile"))]
+    organization = get_organization()
+    version = get_version()  # The version of the stack.
 
     for context in contexts:
-        image = f"{ORG}/{context.name}:{VER}"
+        image = f"{organization}/{context.name}"
 
-        deps = [p for p in context.glob("**/*") if p.is_file()]
+        build_action = ["docker", "build"]
+        build_action.extend([f"-t {image}:{tag}" for tag in get_tags()])
+        build_action.extend(f"--build-arg {arg}" for arg in get_docker_build_args())
+        build_action.append(str(context))
+        build_action = " ".join(build_action)
+
+        deps = ["build.yml", "build.py"] + [
+            p for p in context.glob("**/*") if p.is_file()
+        ]
 
         yield {
-            "name": image,
-            "actions": [f"docker build -t {image} {context}"],
+            "name": f"{image}:{version}",
+            "actions": [build_action],
             "file_dep": deps,
             "verbosity": 2,
         }
