7d6ea0ee8fa7bdc4e0a8be7bfa68d114ca4e321f
Simon Adorf <simon.adorf@epfl.ch>
2022-08-25 18:04:32 +0200

    Refactor into multiples images based on jupyter/minimal-notebook (#254)
    * Major revision that refactors the previous single image into a base and a lab
      image, where the former contains only AiiDA and the latter adds the AiiDAlab
      related software.
    * The PGSQL and RabbitMQ services are expected to be started in separate
      containers, e.g., with docker-compose.
    * There is only a single Python environment with shared dependencies, unlike
      the previous separation with one environment for the Jupyter server and the
      other one for the AiiDA Python kernels.
    * Some requirements, e.g., the installed AiiDA version are pinned for both
      conda/mamba and pip to prevent the user from inadvertently breaking the
      environment; the user can override this or alternatively create a dedicated
      environment.
    * Add tests that are implemented with pytest.
    * Common repository tasks such as building the stack, running tests, and
      starting a lab server can be run with pydoit.
    * Supports Python 3.9.2 and AiiDA 2.0.0.
    
    Co-authored-by: Aliaksandr Yakutovich <yakutovicha@gmail.com>

---
+++ dodo.py
@@ -0,0 +1,62 @@
+from pathlib import Path
+
+from build import get_docker_build_args, get_organization, get_tags, get_version
+
+DOIT_CONFIG = {"default_tasks": ["build"]}
+
+
+def task_build():
+    """Build all docker images."""
+
+    contexts = [p.parent for p in sorted(Path("stack").glob("*/Dockerfile"))]
+    organization = get_organization()
+    version = get_version()  # The version of the stack.
+
+    deps = ["build.yml", "build.py"] + [
+        p for p in Path("stack").glob("**/*") if p.is_file()
+    ]
+
+    for context in contexts:
+        image = f"{organization}/{context.name}"
+
+        build_action = ["docker", "build"]
+        build_action.extend([f"-t {image}:{tag}" for tag in get_tags()])
+        build_action.extend(f"--build-arg {arg}" for arg in get_docker_build_args())
+        build_action.append(str(context))
+        build_action = " ".join(build_action)
+
+        yield {
+            "name": f"{image}:{version}",
+            "actions": [build_action],
+            "file_dep": deps,
+            "verbosity": 2,
+        }
+
+
+def task_tests():
+    """Run tests with pytest."""
+
+    return {"actions": ["pytest -v"], "verbosity": 2}
+
+
+def task_up():
+    """Start AiiDAlab server for testing."""
+    return {
+        "actions": ["AIIDALAB_PORT=%(port)i docker-compose up --detach"],
+        "params": [
+            {
+                "name": "port",
+                "short": "p",
+                "long": "port",
+                "type": int,
+                "default": 8888,
+                "help": "Specify the AiiDAlab host port.",
+            },
+        ],
+        "verbosity": 2,
+    }
+
+
+def task_down():
+    """Stop AiiDAlab server."""
+    return {"actions": ["docker-compose down"], "verbosity": 2}
