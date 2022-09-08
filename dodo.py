71fda7cc165fa65cf980747af94a4312747de5fa
Simon Adorf <carl.simon.adorf@gmail.com>
2022-09-08 15:07:40 +0200

    Use the GitHub Container registry.

--- dodo.py
+++ dodo.py
@@ -2,23 +2,42 @@ from pathlib import Path
 
 DOIT_CONFIG = {"default_tasks": ["build"]}
 
+_REGISTRY_PARAM = {
+    "name": "registry",
+    "short": "r",
+    "long": "registry",
+    "type": str,
+    "default": "docker.io/",
+    "help": "Specify the docker image registry.",
+}
+
 
 def task_build():
     """Build all docker images."""
 
-    return {"actions": ["docker buildx bake"], "verbosity": 2}
+    return {
+        "actions": ["docker buildx bake"],
+        "params": [_REGISTRY_PARAM],
+        "verbosity": 2,
+    }
 
 
 def task_tests():
     """Run tests with pytest."""
 
-    return {"actions": ["pytest -v"], "verbosity": 2}
+    return {
+        "actions": ["REGISTRY=%(registry)s pytest -v"],
+        "params": [_REGISTRY_PARAM],
+        "verbosity": 2,
+    }
 
 
 def task_up():
     """Start AiiDAlab server for testing."""
     return {
-        "actions": ["AIIDALAB_PORT=%(port)i docker-compose up --detach"],
+        "actions": [
+            "AIIDALAB_PORT=%(port)i REGISTRY=%(registry)s docker-compose up --detach"
+        ],
         "params": [
             {
                 "name": "port",
@@ -28,6 +47,7 @@ def task_up():
                 "default": 8888,
                 "help": "Specify the AiiDAlab host port.",
             },
+            _REGISTRY_PARAM,
         ],
         "verbosity": 2,
     }
