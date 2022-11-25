6d008d2a685bfc0bc710371d5f36b54afb05e342
Jusong Yu <jusong.yeu@gmail.com>
2022-11-25 12:45:50 +0100

    Add test for path change of pip local bin folder and jupyter config of local packages

--- tests/test_aiidalab.py
+++ tests/test_aiidalab.py
@@ -68,3 +68,15 @@ def test_verdi_status(aiidalab_exec, nb_user):
     output = aiidalab_exec("verdi status", user=nb_user).decode().strip()
     assert "Connected to RabbitMQ" in output
     assert "Daemon is running" in output
+    
+def test_path_local_pip(aiidalab_exec, nb_user):
+    """test that the pip local bin path .local/bin is added to PATH"""
+    output = aiidalab_exec("echo $PATH", user=nb_user).decode()
+    assert f"/home/{nb_user}/.local/bin" in output
+    
+def test_jupyter_config_path(aiidalab_exec, nb_user):
+    """test that the jupyter config path needed for locally 
+    installed notebook extensions (e.g. nglview)
+    """
+    output = aiidalab_exec("echo $JUPYTER_CONFIG_PATH", user=nb_user).decode()
+    assert f"/home/${nb_user}/.local/etc/jupyter" in output
