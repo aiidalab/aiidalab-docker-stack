fc75748584b569489e41b2b2e21ca66979c09f8e
Jusong Yu <jusong.yeu@gmail.com>
2023-05-30 01:13:30 +0200

    Do not install if exist

--- stack/qe/before-notebook.d/71_install-qeapp.sh
+++ stack/qe/before-notebook.d/71_install-qeapp.sh
@@ -3,8 +3,14 @@
 # Debugging.
 set -x
 
-# Install qeapp.
-aiidalab install --yes quantum-espresso==${AIIDALAB_QE_VERSION}
+# Install qeapp if it is not already installed.
+if aiidalab list | grep -q quantum-espresso; then
+    echo "Quantum ESPRESSO app is already installed."
+    exit 0
+else
+    echo "Installing Quantum ESPRESSO app."
+    aiidalab install --yes quantum-espresso==${AIIDALAB_QE_VERSION}
+fi
 
 # Force restart daemon to make sure that the new version is used.
 verdi daemon restart --reset --timeout 30
