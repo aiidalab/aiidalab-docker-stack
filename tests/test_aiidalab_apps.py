202233073f7667e02776bc89d60244f43dbbabf3
Daniel Hollas <daniel.hollas@bristol.ac.uk>
2024-04-19 17:32:10 +0100

    Fix integration tests

--- tests/test_aiidalab_apps.py
+++ tests/test_aiidalab_apps.py
@@ -16,21 +16,25 @@ def skip_if_incompatible_target(target):
 
 @pytest.fixture(scope="function")
 def generate_aiidalab_install_output(aiidalab_exec, nb_user):
+    pkg = None
+
     def _generate_aiidalab_install_output(package_name):
+        nonlocal pkg
+        pkg = package_name
         cmd = f"aiidalab install --yes --pre {package_name}"
+
         output = aiidalab_exec(cmd, user=nb_user).strip()
-
         output += aiidalab_exec("pip check", user=nb_user).strip()
+        return output
 
-        yield output
-        # Now, ninstall the package to make sure the test is repeatable.
-        # NOTE: This will only uninstall the package itself, not its dependencies!
-        # Since the dependencies are installed via pip, this is basically a pip limitation
-        # that would be hard to workaround here.
-        app_name = package_name.split("@")[0]
+    # Uninstall the package to make sure the test is repeatable.
+    # NOTE: This will only uninstall the package itself, not its dependencies!
+    # Since the dependencies are installed via pip, this is basically a pip limitation
+    # that would be hard to workaround here.
+    yield _generate_aiidalab_install_output
+    if pkg:
+        app_name = pkg.split("@")[0]
         aiidalab_exec(f"aiidalab uninstall --yes --force {app_name}", user=nb_user)
-
-    return _generate_aiidalab_install_output
 
 
 @pytest.mark.parametrize("package_name", ["aiidalab-widgets-base", "quantum-espresso"])
