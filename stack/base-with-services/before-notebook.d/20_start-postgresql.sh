b4e61457bc9206e8283f7787bee7a4672baa0ffd
Daniel Hollas <daniel.hollas@bristol.ac.uk>
2024-05-21 02:00:06 +0100

    PGDATA everywhere

--- stack/base-with-services/before-notebook.d/20_start-postgresql.sh
+++ stack/base-with-services/before-notebook.d/20_start-postgresql.sh
@@ -2,29 +2,29 @@
 set -x
 
 # -w waits until server is up
-PSQL_START_CMD="pg_ctl --timeout=180 -w -D /home/${NB_USER}/.postgresql -l /home/${NB_USER}/.postgresql/logfile start"
-PSQL_STOP_CMD="pg_ctl -w -D /home/${NB_USER}/.postgresql stop"
-PSQL_STATUS_CMD="pg_ctl -D /home/${NB_USER}/.postgresql status"
+PSQL_START_CMD="pg_ctl --timeout=180 -w -l ${PGDATA}/logfile start"
+PSQL_STOP_CMD="pg_ctl -w stop"
+PSQL_STATUS_CMD="pg_ctl status"
 
 MAMBA_RUN="mamba run -n aiida-core-services"
 
 # make DB directory, if not existent
-if [ ! -d /home/${NB_USER}/.postgresql ]; then
-   mkdir /home/${NB_USER}/.postgresql
+if [ ! -d ${PGDATA} ]; then
+   mkdir ${PGDATA}
    ${MAMBA_RUN} initdb
-   echo "unix_socket_directories = '/tmp'" >> /home/${NB_USER}/.postgresql/postgresql.conf
+   echo "unix_socket_directories = '/tmp'" >> ${PGDATA}/postgresql.conf
    ${MAMBA_RUN} ${PSQL_START_CMD}
 
 else
     # Fix problem with kubernetes cluster that adds rws permissions to the group
     # for more details see: https://github.com/materialscloud-org/aiidalab-z2jh-eosc/issues/5
-    chmod g-rwxs /home/${NB_USER}/.postgresql -R
+    chmod g-rwxs ${PGDATA} -R
 
     if ! ${MAMBA_RUN} ${PSQL_STATUS_CMD}; then
        # Cleaning up the mess if Postgresql was not shutdown properly.
        # TODO: Rotate logfile
-       echo "" > /home/${NB_USER}/.postgresql/logfile
-       rm -vf /home/${NB_USER}/.postgresql/postmaster.pid
+       echo "" > ${PGDATA}/logfile
+       rm -vf ${PGDATA}/postmaster.pid
        ${MAMBA_RUN} ${PSQL_START_CMD}
    fi
 fi
