2d0e8f47e545e4f30f2261f4530ae5284474e639
Daniel Hollas <daniel.hollas@bristol.ac.uk>
2024-05-26 17:53:36 +0100

    remove pg_ctl status

--- stack/base-with-services/before-notebook.d/20_start-postgresql.sh
+++ stack/base-with-services/before-notebook.d/20_start-postgresql.sh
@@ -1,27 +1,32 @@
 #!/bin/bash
 set -x
 
+if [[ -z $PGDATA ]]; then
+   echo "ERROR: PGDATA variable not set, cannot start PostgreSQL!"
+   exit 1
+fi
+
+PSQL_LOGFILE="${PGDATA}/logfile"
 # -w waits until server is up
-PSQL_START_CMD="pg_ctl --timeout=180 -w -l ${PGDATA}/logfile start"
-PSQL_STATUS_CMD="pg_ctl status"
+PSQL_START_CMD="pg_ctl --timeout=180 -w -l ${PSQL_LOGFILE} start"
 
 MAMBA_RUN="mamba run -n aiida-core-services"
 
-# make DB directory, if not existent
-if [ ! -d ${PGDATA} ]; then
-   mkdir ${PGDATA}
-   ${MAMBA_RUN} initdb
-   echo "unix_socket_directories = '/tmp'" >> ${PGDATA}/postgresql.conf
-   ${MAMBA_RUN} ${PSQL_START_CMD}
-
+# Initialize DB directory if it does not exist
+if [[ ! -d ${PGDATA} ]]; then
+    mkdir "${PGDATA}"
+    ${MAMBA_RUN} initdb
+    echo "unix_socket_directories = '/tmp'" >> "${PGDATA}/postgresql.conf"
 else
     # Fix problem with kubernetes cluster that adds rws permissions to the group
-    chmod -R g-rwxs ${PGDATA}
+    chmod -R g-rwxs "${PGDATA}"
 
-    if ! ${MAMBA_RUN} ${PSQL_STATUS_CMD}; then
-       # Cleaning up the mess if Postgresql was not shutdown properly.
-       mv ${PGDATA}/logfile ${PGDATA}/logfile.1 && gzip ${PGDATA}/logfile.1
-       rm -vf ${PGDATA}/postmaster.pid
-       ${MAMBA_RUN} ${PSQL_START_CMD}
-   fi
+    if [[ -f ${PGDATA}/logfile ]]; then
+         mv "${PSQL_LOGFILE}" "${PSQL_LOGFILE}.1" && gzip "${PSQL_LOGFILE}.1"
+    fi
+    # Cleaning up the mess if Postgresql was not shutdown properly.
+    rm -vf "${PGDATA}/postmaster.pid"
 fi
+
+# Start the server
+${MAMBA_RUN} ${PSQL_START_CMD}
