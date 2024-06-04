53b72fee00237fd790436e3ee587c6b8be734a88
Jusong Yu <jusong.yeu@gmail.com>
2024-06-04 14:18:37 +0200

    A valide hq setup

--- stack/base/before-notebook.d/45_start_hq_setup_qe_blob.sh
+++ stack/base/before-notebook.d/45_start_hq_setup_qe_blob.sh
@@ -10,4 +10,18 @@ nohup hq server start 1>$HOME/.hq-stdout 2>$HOME/.hq-stderr &
 # 2. if it is a good idea just give the 4 fake CPUs. (personally like this setup, for large computation it will anyway goes to the HPC.)
 mkdir .hq-server
 nohup hq worker start --cpus=2 --resource "mem=sum(2560)" --no-detect-resources &
+sleep 2
 nohup hq worker start --cpus=2 --resource "mem=sum(2560)" --no-detect-resources &
+
+# Setup the pw code
+verdi code create core.code.installed --non-interactive \
+  --label pw-7.2 \
+  --description "pw-7.2 run on hq local" \
+  --default-calc-job-plugin quantumespresso.pw \
+  --computer localhost-hq \
+  --prepend-text 'eval "$(conda shell.posix hook)"
+conda activate base
+export OMP_NUM_THREADS=1' \
+  --filepath-executable pw.x
+
+aiida-pseudo install sssp --functional PBE -p precision
