401050927dd16d3115f546c0f870f3a4661ade2a
Daniel Hollas <daniel.hollas@bristol.ac.uk>
2024-06-10 23:47:49 +0200

    conftest: Print container logs if notebook service fails to start

--- tests/conftest.py
+++ tests/conftest.py
@@ -64,9 +64,13 @@ def notebook_service(docker_ip, docker_services):
     """Ensure that HTTP service is up and responsive."""
     port = docker_services.port_for("aiidalab", 8888)
     url = f"http://{docker_ip}:{port}"
-    docker_services.wait_until_responsive(
-        timeout=60.0, pause=0.1, check=lambda: is_responsive(url)
-    )
+    try:
+        docker_services.wait_until_responsive(
+            timeout=60.0, pause=0.1, check=lambda: is_responsive(url)
+        )
+    except Exception:
+        print(docker_compose.execute("logs").decode().strip())
+        raise
     return url
 
 
