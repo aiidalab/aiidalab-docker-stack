917ca0e24a17d8d2db0ebfa0c38be9e46494da88
Daniel Hollas <danekhollas@gmail.com>
2024-06-11 10:30:50 +0200

    Increase minimum job poll interval to reduce verdi daemon CPU usage (#466)
    Set the job poll interval of 2 seconds. This should keep the CPU utilization below 10% according to @cpignedoli benchmarks in #462. At the same time, 5s felt too long, since users might want to run super quick calculations to try things out. 
    AiiDA not yet provide API to set the interval, so the python code is directly inject to the start up script to set.

--- tests/conftest.py
+++ tests/conftest.py
@@ -62,11 +62,19 @@ def docker_compose_file(pytestconfig):
 @pytest.fixture(scope="session")
 def notebook_service(docker_ip, docker_services):
     """Ensure that HTTP service is up and responsive."""
+
+    # using `docker_compose` fixture would trigger a separate container
+    docker_compose = docker_services._docker_compose
     port = docker_services.port_for("aiidalab", 8888)
     url = f"http://{docker_ip}:{port}"
-    docker_services.wait_until_responsive(
-        timeout=60.0, pause=0.1, check=lambda: is_responsive(url)
-    )
+    try:
+        docker_services.wait_until_responsive(
+            timeout=60.0, pause=0.1, check=lambda: is_responsive(url)
+        )
+    except Exception as e:
+        print(docker_compose.execute("logs").decode().strip())
+        # Let's exit hard, otherwise pytest output is a huge mess.
+        pytest.exit(e)
     return url
 
 
