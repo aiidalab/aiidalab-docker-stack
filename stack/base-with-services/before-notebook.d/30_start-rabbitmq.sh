d998c9b5c7e533060fa333bb54c015a7161b55f9
Daniel Hollas <daniel.hollas@bristol.ac.uk>
2024-06-11 14:46:30 +0200

    Add a note

--- stack/base-with-services/before-notebook.d/30_start-rabbitmq.sh
+++ stack/base-with-services/before-notebook.d/30_start-rabbitmq.sh
@@ -4,4 +4,7 @@ set -emx
 # Fix issue where the erlang cookie permissions are corrupted.
 chmod 400 "/home/${NB_USER}/.erlang.cookie" || echo "erlang cookie not created yet."
 
+# NOTE: In arm64 build, rabbitmq is not installed via conda,
+# but the following incantation still works since
+# rabbitmq-server is available globally.
 mamba run -n aiida-core-services rabbitmq-server -detached
