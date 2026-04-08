d9609f9eb4ae5a9ec3541368881adc93f31f2d1a
Aliaksandr Yakutovich <yakutovicha@gmail.com>
2026-04-08 10:18:57 +0200

    start_rabbitmq.sh: removed sleep 2 and the outdated arm64 comment

--- stack/base-with-services/before-notebook.d/30_start-rabbitmq.sh
+++ stack/base-with-services/before-notebook.d/30_start-rabbitmq.sh
@@ -8,10 +8,4 @@ chmod 400 "/home/${NB_USER}/.erlang.cookie" || echo "erlang cookie not created y
 eval "$(conda shell.bash hook)"
 conda activate aiida-core-services
 
-# NOTE: In arm64 build, rabbitmq is not installed via conda,
-# but the following incantation still works since
-# rabbitmq-server is available globally.
 rabbitmq-server -detached
-
-# Wait a moment for RabbitMQ to start
-sleep 2
